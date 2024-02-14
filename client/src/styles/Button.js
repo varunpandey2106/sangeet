ba14a1ffa12690a03fb2fdf85dd63617513e266b
varunpandey2106 <varun.pandey2106@gmail.com>
2024-02-15 03:26:50 +0530

    button .js

--- client/src/styles/Button.js
+++ client/src/styles/Button.js
@@ -15,3 +15,13 @@ const Button = styled.button`
 `;
 
 export default Button;
+
+
+
+// font-size: Sets the font size to the base font size defined in the theme.
+// cursor: Sets the cursor to pointer to indicate interactivity.
+// border: Removes the default button border.
+// border-radius: Sets the border radius to 0 to remove any rounded corners.
+// transition: Applies a transition effect defined in the theme to provide smooth transitions for properties that change over time.
+// &:focus, &:active: Defines styles for the button when it's focused or active (clicked).
+// outline: Removes the default focus outline.
