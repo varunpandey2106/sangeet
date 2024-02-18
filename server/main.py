15a5dd59b1d6a4abc742529a2f7cb10d90c98acc
varunpandey2106 <varun.pandey2106@gmail.com>
2024-02-18 22:23:02 +0530

    prod changes

--- server/main.py
+++ server/main.py
@@ -28,7 +28,7 @@ app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, 
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://sangeetclient-ahhsjefha-varunux.vercel.app"],
     allow_credentials=True,
     allow_methods=["GET", "POST"],
     allow_headers=["Authorization", "Content-Type"],
