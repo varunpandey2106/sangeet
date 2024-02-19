2117f91d2e19048903e66f5298dfd06e896a6427
varunpandey2106 <varun.pandey2106@gmail.com>
2024-02-19 20:35:44 +0530

    cors

--- server/main.py
+++ server/main.py
@@ -30,7 +30,7 @@ app.add_middleware(SessionMiddleware, secret_key=os.urandom(32))
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://sangeetapi.vercel.app",  ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST"],
     allow_headers=["Authorization", "Content-Type"],
